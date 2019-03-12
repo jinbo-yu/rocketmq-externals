a0bc7a2ae01f52509a5bb012fe9c07de79ff5b1d
余金波 <jinbo.yujb@gmail.com>
2019-03-12 13:24:27 +0800

    remove csrf

--- rocketmq-console/src/main/java/org/apache/rocketmq/console/config/WebSecurityConfig.java
+++ rocketmq-console/src/main/java/org/apache/rocketmq/console/config/WebSecurityConfig.java
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -55,6 +56,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 udb.and().withUser(u.getUser()).password(u.getPassword()).roles(role);
             }
         }
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().and()
+            .httpBasic();
+        http.csrf().disable();
     }
 }
 
